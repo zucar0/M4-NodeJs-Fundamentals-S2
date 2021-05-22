d73cb6dbf6f13d686ae6360729489c2cbd73e471
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-22 18:16:27 -0500

    descargar imagen con nombre dinamico

--- serve_nasa.js
+++ serve_nasa.js
@@ -2,6 +2,21 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
+// function descargarImagen(url, localPath){
+//     var fullUrl = url;
+//     var file = fs.createWriteStream(localPath);
+//     var request = https.get(url, function(response){
+//         response.pipe(file);
+//     });
+// }
+
+function descargarImagen(url, imageName){
+    let file = fs.createWriteStream(path.join(__dirname, '', imageName + ".png"));
+    https.get(url, function(response){
+        response.pipe(file);
+    });
+}
+
 function getApi(){
     https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (res) =>{
         let data ='';
@@ -17,7 +32,11 @@ function getApi(){
             console.log('Título ', body.title);
             console.log('\nExplicación ', body.explanation);
             console.log('\nUrl ', body.url );
+            //Se manda llamada la función para descargar la imagen
+            // descargarImagen(body.url, path.join(__dirname, body.title));
+            descargarImagen(body.url,body.title);
         })
+
     }).on('error', (err) => {
         console.log('Error ', err);
     })
