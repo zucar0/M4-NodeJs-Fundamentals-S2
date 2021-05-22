5b6e0baa3ec69410d42a1df88ba009375fbca0cb
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-22 13:13:55 -0500

    http

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log(__dirname);
+// console.log(__di rname);
+//Importar el módulo de http (Ya viene instalado con los módulos de node)
+const http = require('http');
+//Se crea el servidor que recibe una función
+let server = http.createServer(function(req, res){
+    res.end('¡Hola, mundo!')
+})
+//Servidor que dentro lleva una funcion
+server.listen(3000, function(){
+    console.log("Servidor corriendo en", 3000); 
+})
