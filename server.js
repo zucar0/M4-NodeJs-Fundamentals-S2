3bd8a50295f3708c2d32bfdd87701d41e105eb24
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-22 14:07:29 -0500

    pruebas con archivos

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const path=require('path');
 //Importación del módulo fs
  const fs = require('fs');
 
- fs.writeFile(path.join(__dirname,'/ejemplo2.txt'),'Este es el texto dos', {encoding:'utf-8'},(err)=>{
+ fs.writeFile(path.join(__dirname,'/ejemplo4.txt'),'Este es el texto 4', {encoding:'utf-8'},(err)=>{
      if(err) throw err;
      console.log('¡Archivo creado!');
  });
 
- fs.writeFile(path.join(__dirname,'/ejemplo2.pdf'),'Este es el texto dos', {encoding:'utf-8'},(err)=>{
-    if(err) throw err;
-    console.log('¡Archivo creado!');
+
+//Lectura del archivo
+fs.readFile(path.join(__dirname,'/ejemplo4.txt'), {encoding:'utf-8'},(err, data)=>{
+    if(err) return console.log (err);
+    console.log('Leer archivo: ', data);
 });
 
+//Agregar información a un archivo ya creado
+fs.appendFile(path.join(__dirname, '/ejemplo4.txt'), '\nSegunda línea', {encoding: 'utf-8'}, (err)=> {
+    if(err) throw err;
+    console.log('Archivo actualizado.')
+})
